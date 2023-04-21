6a94c55d8f398a9c9a8ce215192b9891b584766f
duweiwei <duqings@foxmail.com>
2023-04-22 02:17:41 +0800

    empty project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped></style>
